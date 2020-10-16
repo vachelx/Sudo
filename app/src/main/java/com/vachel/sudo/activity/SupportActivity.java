6c643ae82210f9b7ce7d906daf2adf6292d02b66
jianglixuan <jiang.lixuan@westone.com.cn>
2020-10-16 10:52:42 +0800

    support zfb
    Signed-off-by: jianglixuan <jiang.lixuan@westone.com.cn>

--- app/src/main/java/com/vachel/sudo/activity/SupportActivity.java
+++ app/src/main/java/com/vachel/sudo/activity/SupportActivity.java
@@ -2,10 +2,13 @@ package com.vachel.sudo.activity;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.Intent;
 import android.view.View;
 
 import com.vachel.sudo.R;
 import com.vachel.sudo.utils.ToastUtil;
+
+import java.net.URISyntaxException;
 
 public class SupportActivity extends BaseActivity implements View.OnClickListener {
     @Override
@@ -40,12 +43,31 @@ public class SupportActivity extends BaseActivity implements View.OnClickListene
             return true;
         });
         findViewById(R.id.back_icon).setOnClickListener(this);
+        findViewById(R.id.support_pay).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.back_icon) {
             onBackPressed();
+        }else if (v.getId() == R.id.support_pay){
+            jumpToSupport();
         }
     }
+
+    private void jumpToSupport(){
+        try {
+            String intentUrl = URL_FORMAT.replace("{urlCode}", "fkx19104h39ewbn4uuq75ab");
+            Intent intent = Intent.parseUri(intentUrl, Intent.URI_INTENT_SCHEME);
+            startActivity(intent);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static final String URL_FORMAT =
+            "intent://platformapi/startapp?saId=10000007&" +
+                    "clientVersion=3.7.0.0718&qrcode=https%3A%2F%2Fqr.alipay.com%2F{urlCode}%3F_s" +
+                    "%3Dweb-other&_t=1472443966571#Intent;" + "scheme=alipayqr;package=com.eg.android.AlipayGphone;end";
 }
